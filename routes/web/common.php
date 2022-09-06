089a05a26806c32ed9d82a91e87998e17fdafb8b
alaa <alaashawa1995@gmail.com>
2022-09-06 11:17:12 +0300

    ggg

--- routes/web/common.php
+++ routes/web/common.php
@@ -13,7 +13,7 @@
  * Temporary dummy route for testing SPA.
  */
 
-Route::namespace ('Common')->group(function () {
+Route::namespace ('Common')->group(['middleware' => 'Localization'], function () {
 
 	// Get admin-login form
 	Route::get('unauthorized', 'ExceptionViewController@unauthorized');
